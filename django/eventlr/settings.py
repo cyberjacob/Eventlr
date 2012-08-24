c2c7a66ece28ed16e933fabe74263ed61921c3ea
Jacob Mansfield <cyberjacob@gmail.com>
2012-08-24 21:25:42 +0100

    added AWS keys

--- django/eventlr/settings.py
+++ django/eventlr/settings.py
@@ -146,9 +146,8 @@ LOGGING = {
 }
 
 
-AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
-AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = 'static.eventlr.co.uk'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
