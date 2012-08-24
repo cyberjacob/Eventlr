438e2150be5f6d8dc2519d7815e4cf59efbf2126
Jacob Mansfield <cyberjacob@gmail.com>
2012-08-25 00:31:43 +0100

    fixed static folder location

--- django/web/pages.py
+++ django/web/pages.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def index(request):
-    page = """ <h1>Hello, World!</h1><img src="{{ STATIC_URL }}vetlogo.jpg" /> """
+    page = """ <h1>Hello, World!</h1><img src="""+"{{ STATIC_URL }}"+"""vetlogo.jpg" /> """
     return HttpResponse(page)
