1c1b66f3fa47c4a0386628fe02bf389f644cdb7f
Jacob Mansfield <cyberjacob@gmail.com>
2012-08-25 19:01:22 +0100

    changed template loaders

--- django/eventlr/settings.py
+++ django/eventlr/settings.py
@@ -42,7 +42,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+#MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -53,11 +53,11 @@ MEDIA_URL = 'http://static.eventlr.co.uk/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://static.eventlr.co.uk'
+STATIC_URL = 'http://static.eventlr.co.uk/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
